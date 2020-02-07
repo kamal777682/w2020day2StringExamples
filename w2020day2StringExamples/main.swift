d81bf6d87b2abdfd8df8bcdec64787791cb497bf
kamal <kamalpreetkaur2098@gmail.com>
2020-02-06 21:05:25 -0500

    initial commit

--- w2020day2StringExamples/main.swift
+++ w2020day2StringExamples/main.swift
@@ -71,4 +71,95 @@ print(name.dropLast())
 print (name.randomElement)
 
 let five = "5" as Character
-print(five.isSymbol)
+print(name.first)
+print(name.last)
+var aa1 : String?
+    aa1 = "hello"
+    aa1 = nil
+
+print(aa1 ?? " NO Value")
+
+if let x = aa1
+{
+    print(x.lowercased())
+}
+else
+{
+    print("aa1 is nil")
+}
+//print(aa1!.lowercased())
+
+for i in 1...10
+{
+    print("\(i) kamalpreet")
+}
+
+for i in 1..<10
+{
+    print(i)
+}
+
+for i in stride(from: 2, to: 10, by: 2)
+{
+    print(i)
+}
+ for i in stride(from: 10, to: 0, by: -1)
+{
+    print(i)
+}
+
+for i in stride(from: 10, through: 1, by: -1)
+
+{
+    print(i)
+}
+
+var country = ["INDIA","CANADA","AUSTRALIA","NEWZEALAND"]
+
+for c in country
+{
+    print(c)
+}
+ print(country[0])
+print(country.count)
+
+country.append("SRI LANKA")
+
+for c in country
+{
+    print(c)
+}
+
+country += ["Nepal","Japan"]
+
+print(country)
+
+var c1 = country[2...3]
+print(c1)
+ 
+c1[3] = "Hello"
+print(c1)
+print(country)
+
+var x = Array(repeating: 02, count: 10)
+print(type(of: country))
+
+var z  = Array<Double>()
+
+print(type(of: z))
+
+z.append(1000)
+z.append(2000)
+z.append(300)
+print(z)
+print(z.endIndex)
+print (z.remove(at: 2))
+print(z)
+
+
+var y : [Int]
+ 
+y = [Int]()
+y.append(10)
+
+print(y)
